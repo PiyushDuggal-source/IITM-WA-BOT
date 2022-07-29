2d2fb9b0f058118e484d735fa11340e35b13b74c
PiyushDuggal-source <duggalpiyush0@gmail.com>
2022-07-29 17:42:25 +0530

    added remoteAuth

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import * as WAWebJS from "whatsapp-web.js";
 import {
   Client,
   GroupNotification,
-  LocalAuth,
   MessageMedia,
+  RemoteAuth,
 } from "whatsapp-web.js";
 import qrcode = require("qrcode-terminal");
 import { checkMessage } from "./actions/messageActions";
@@ -19,87 +19,103 @@ const express = require("express");
 import axios from "axios";
 import * as dotenv from "dotenv";
 import { Request, Response } from "express";
-dotenv.config({ path: __dirname + "/.env" });
+import mongoose from "mongoose";
+import { MongoStore } from "wwebjs-mongo";
+dotenv.config();
 
 const app = express();
-const client = new Client({
-  puppeteer: {
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  },
-  authStrategy: new LocalAuth({
-    dataPath: `${__dirname}/sessions`,
-  }),
-});
 
-client.on("qr", (qr: string) => {
-  qrcode.generate(qr, { small: true });
-  console.log(qr);
-});
+mongoose.connect(process.env.DB_URL as string, () => {
+  const store = new MongoStore({ mongoose: mongoose });
+  const client = new Client({
+    puppeteer: {
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    },
+    authStrategy: new RemoteAuth({
+      store: store,
+      backupSyncIntervalMs: 300000,
+    }),
+  });
 
-client.on("ready", async () => {
-  console.log("Connected");
-});
+  // const client = new Client({
+  //   puppeteer: {
+  //     headless: true,
+  //     args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  //   },
+  //   authStrategy: new LocalAuth({
+  //     dataPath: `${__dirname}/sessions`,
+  //   }),
+  // });
 
-client.on("message_create", async (message: WAWebJS.Message) => {
-  const bool = checkMessage(message);
-  const str: string[] = message.mentionedIds;
-  const isMention =
-    (message.body[0] === "@" && str.includes("919871453667@c.us")) ||
-    message.body
-      .toLowerCase()
-      .split(" ")
-      .includes(`@${(process.env.BOT_NAME as String).toLocaleLowerCase()}`);
-  if (isMention) {
-    const allChats = await client.getChats();
-    const WA_BOT = allChats[0];
-    if (bool === "ADMIN") {
-      introduction(WA_BOT, true);
-    } else if (bool === "USER") {
-      introduction(WA_BOT, false);
+  client.on("qr", (qr: string) => {
+    qrcode.generate(qr, { small: true });
+    console.log(qr);
+  });
+
+  client.on("ready", async () => {
+    console.log("Connected");
+  });
+
+  client.on("message_create", async (message: WAWebJS.Message) => {
+    const bool = checkMessage(message);
+    const str: string[] = message.mentionedIds;
+    const isMention =
+      (message.body[0] === "@" && str.includes("919871453667@c.us")) ||
+      message.body
+        .toLowerCase()
+        .split(" ")
+        .includes(`@${(process.env.BOT_NAME as String).toLocaleLowerCase()}`);
+    if (isMention) {
+      const allChats = await client.getChats();
+      const WA_BOT = allChats[0];
+      if (bool === "ADMIN") {
+        introduction(WA_BOT, true);
+      } else if (bool === "USER") {
+        introduction(WA_BOT, false);
+      }
     }
-  }
-  if (
-    (bool === "ADMIN" || bool === "USER") &&
-    message.body[0] === (process.env.BOT_PREFIX as string)
-  ) {
+    if (
+      (bool === "ADMIN" || bool === "USER") &&
+      message.body[0] === (process.env.BOT_PREFIX as string)
+    ) {
+      const allChats = await client.getChats();
+      const WA_BOT = allChats[0];
+      main(WA_BOT, message, bool);
+    }
+  });
+
+  client.on("group_join", async (msg: GroupNotification) => {
+    const contact = await client.getNumberId(msg.recipientIds[0]);
+    const details = await client.getContactById(contact?._serialized || "");
+    if (details.name) {
+      client.sendMessage(
+        process.env.WA_BOT_ID as string,
+        `${process.env.BOT_NAME as String}: *${
+          details.name
+        }* Joined the Group!\n${
+          USER_JOIN_GREETINGS.messages[random(USER_JOIN_GREETINGS.messageNum)]
+        }\nHey new ${GREETINGS.member[random(GREETINGS.memberMsgNumber)]} ${
+          HEY_EMOJIES[random(HEY_EMOJIES.length)]
+        }!\nCheck out what bot(${
+          process.env.BOT_NAME as String
+        }) can do by *Mentioning* me!\nor check the Commands of ${
+          process.env.BOT_NAME as String
+        } by typing "!cmd" (without quotes)`
+      );
+    }
+  });
+
+  client.on("group_leave", async () => {
     const allChats = await client.getChats();
     const WA_BOT = allChats[0];
-    main(WA_BOT, message, bool);
-  }
+    const sticker = MessageMedia.fromFilePath(`${__dirname}/leave.png`);
+    WA_BOT.sendMessage(`${process.env.BOT_NAME as String}:`);
+    WA_BOT.sendMessage(sticker, { sendMediaAsSticker: true });
+  });
+
+  client.initialize();
 });
-
-client.on("group_join", async (msg: GroupNotification) => {
-  const contact = await client.getNumberId(msg.recipientIds[0]);
-  const details = await client.getContactById(contact?._serialized || "");
-  if (details.name) {
-    client.sendMessage(
-      process.env.WA_BOT_ID as string,
-      `${process.env.BOT_NAME as String}: *${
-        details.name
-      }* Joined the Group!\n${
-        USER_JOIN_GREETINGS.messages[random(USER_JOIN_GREETINGS.messageNum)]
-      }\nHey new ${GREETINGS.member[random(GREETINGS.memberMsgNumber)]} ${
-        HEY_EMOJIES[random(HEY_EMOJIES.length)]
-      }!\nCheck out what bot(${
-        process.env.BOT_NAME as String
-      }) can do by *Mentioning* me!\nor check the Commands of ${
-        process.env.BOT_NAME as String
-      } by typing "!cmd" (without quotes)`
-    );
-  }
-});
-
-client.on("group_leave", async () => {
-  const allChats = await client.getChats();
-  const WA_BOT = allChats[0];
-  const sticker = MessageMedia.fromFilePath(`${__dirname}/leave.png`);
-  WA_BOT.sendMessage(`${process.env.BOT_NAME as String}:`);
-  WA_BOT.sendMessage(sticker, { sendMediaAsSticker: true });
-});
-
-client.initialize();
-
 // Get Bot LIVE
 
 // Continuously ping the server to prevent it from becoming idle
