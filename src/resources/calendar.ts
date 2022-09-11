ce499ddfcd0c4ff672f95af444b29dd44da93584
PiyushDuggal-source <duggalpiyush0@gmail.com>
2022-09-11 23:13:57 +0530

    added calendars

--- src/resources/calendar.ts
+++ src/resources/calendar.ts
@@ -61,6 +61,12 @@ export const CALENDAR: Calendar = [
     courseName: "Mathematics for Data Science-1 - May - 2022",
   },
   {
+    topic: "Office Hours - Maths 1",
+    date: new Date("Monday, September 12, 2022, 14:00"),
+    time: "2:00 – 4:00pm",
+    courseName: "Mathematics for Data Science-1 - May - 2022",
+  },
+  {
     topic: "Solve with Us - Week 1",
     date: new Date("Tuesday, September 13, 2022,18:00"),
     time: "6:00 – 8:00pm",
@@ -77,6 +83,48 @@ export const CALENDAR: Calendar = [
     date: new Date("Tuesday, September 13, 2022, 20:00"),
     time: "8:00 – 10:00pm",
     courseName: "Statistics-1 - May - 2022",
+  },
+  {
+    topic: "Office Hours - Maths 1",
+    date: new Date("Wednesday, September 14, 2022, 14:00"),
+    time: "2:00 – 4:00pm",
+    courseName: "Mathematics for Data Science-1 - May - 2022",
+  },
+  {
+    topic: "Activity/Practice Session - Week 2",
+    date: new Date("Thursday, September 15, 2022, 7:00"),
+    time: "7:00 – 9:00am",
+    courseName: "Mathematics for Data Science-1 - May - 2022",
+  },
+  {
+    topic: "Extra Live Session - Week 2",
+    date: new Date("Friday, September 16, 2022, 7:00"),
+    time: "7:00 – 9:00am",
+    courseName: "Mathematics for Data Science-1 - May - 2022",
+  },
+  {
+    topic: "Office Hours - Maths 1",
+    date: new Date("Friday, September 16, 2022, 14:00"),
+    time: "2:00 – 4:00pm",
+    courseName: "Mathematics for Data Science-1 - May - 2022",
+  },
+  {
+    topic: "Open Session Week 2 (CT)",
+    date: new Date("Friday, September 16, 2022, 18:00"),
+    time: "6:00 – 8:00pm",
+    courseName: "Computational Thinking - May - 2022",
+  },
+  {
+    topic: "Extra Live Session - Week 2 (1)",
+    date: new Date("Saturday, September 17, 2022, 14:00"),
+    time: "2:00 – 4:00pm",
+    courseName: "Mathematics for Data Science-1 - May - 2022",
+  },
+  {
+    topic: "Office Hours - Maths 1",
+    date: new Date("Sunday, September 18, 2022, 14:00"),
+    time: "2:00 – 4:00pm",
+    courseName: "Mathematics for Data Science-1 - May - 2022",
   },
   {
     topic: "Read with us – A Virtual Reading Session",
