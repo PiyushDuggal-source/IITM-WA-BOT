ff375be246dbcace030462089472a090fd1a9c10
Piyush Duggal <duggalpiyush0@gmail.com>
2022-12-16 20:35:32 +0530

    added latest calendar events

--- src/resources/calendar.ts
+++ src/resources/calendar.ts
@@ -7,93 +7,9 @@ import { Calendar } from "../types/types";
 
 export const CALENDAR: Calendar = [
   {
-    topic: "Solve with Us - Week 7 (Maths I)",
-    date: new Date("Tuesday, October 25, 2022, 18:00"),
-    time: "6:00 – 8:00pm",
-    courseName: "Mathematics for Data Science-1 - May - 2022",
-  },
-  {
-    topic: "Solve with Us - Week 7 (Statistics I)",
-    date: new Date("Tuesday, October 25, 2022, 20:00"),
-    time: "8:00 – 10:00pm",
-    courseName: "Statistics-1 - May - 2022",
-  },
-  {
-    topic: "Open Session - Week 7 (Maths I)",
-    date: new Date("Wednesday, October 26, 2022, 07:00"),
-    time: "7:00 – 9:00am",
-    courseName: "Mathematics for Data Science-1 - May - 2022",
-  },
-  {
-    topic: "Doubt Clearing Session - Maths 1",
-    date: new Date("Wednesday, October 26, 2022, 19:00"),
-    time: "7:00 – 9:00pm",
-    courseName: "Mathematics for Data Science-1 - May - 2022",
-  },
-  {
-    topic: "Activity/Practice Session - Week 7 (Maths I)",
-    date: new Date("Thursday, October 27, 2022, 07:00"),
-    time: "7:00 – 9:00am",
-    courseName: "Mathematics for Data Science-1 - May - 2022",
-  },
-  {
-    topic: "Solve with US - Week 7 (CT)",
-    date: new Date("Thursday, October 27, 2022, 17:00"),
-    time: "5:00 – 7:00pm",
-    courseName: "Mathematics for Data Science-1 - May - 2022",
-  },
-  {
-    topic: "Open Session Week 7 (CT)",
-    date: new Date("Friday, October 28, 2022, 18:00"),
-    time: "6:00 – 8:00pm",
-    courseName: "Computational Thinking - May - 2022",
-  },
-  {
-    topic: "Open Session Week 7 (CT)",
-    date: new Date("Sunday, October 30, 2022, 18:00"),
-    time: "6:00 – 8:00pm",
-    courseName: "Statistics-1 - May - 2022",
-  },
-  {
-    topic: "Open Session Week 7 (CT)",
-    date: new Date("Monday, October 31, 2022, 18:00"),
-    time: "6:00 – 8:00pm",
-    courseName: "Computational Thinking - May - 2022",
-  },
-  {
-    topic: "Open Session - Week 8 (Maths I)",
-    date: new Date("Monday, October 31, 2022, 20:00"),
-    time: "8:00 – 10:00pm",
-    courseName: "Mathematics for Data Science-1 - May - 2022",
-  },
-  {
-    topic: "Open Session Week 8 (CT)",
-    date: new Date("Tuesday, November 1, 2022, 16:00"),
-    time: "4:00 – 6:00pm",
-    courseName: "Computational Thinking - May - 2022",
-  },
-  {
-    topic: "Solve with Us - Week 8 (Maths I)",
-    date: new Date("Tuesday, November 1, 2022, 18:00"),
-    time: "6:00 – 8:00pm",
-    courseName: "Mathematics for Data Science-1 - May - 2022",
-  },
-  {
-    topic: "Solve with Us - Week 8 (Statistics I)",
-    date: new Date("Tuesday, November 1, 2022, 20:00"),
-    time: "8:00 – 10:00pm",
-    courseName: "Statistics-1 - May - 2022",
-  },
-  {
-    topic: "Doubt Clearing Session - Maths 1",
-    date: new Date("Wednesday, November 2, 2022, 19:00"),
-    time: "7:00 – 9:00pm",
-    courseName: "Mathematics for Data Science-1 - May - 2022",
-  },
-  {
-    topic: "Activity/Practice Session - Week 8 (Maths I)",
-    date: new Date("Thursday, November 3, 2022, 07:00"),
-    time: "7:00 – 9:00am",
-    courseName: "Mathematics for Data Science-1 - May - 2022",
-  },
+    courseName: "Student Academic Calendar",
+    date: new Date("Sunday, December 18, 2022, 19:00"),
+    time: "7:00 – 8:00pm",
+    topic: "Read with us – A Virtual Reading Session"
+  }
 ];
