80c16484b22da908f641c99b98752f6ff2f70eb4
Piyush Duggal <duggalpiyush0@gmail.com>
2023-01-11 05:10:19 +0530

    added field for implementing #13

--- src/modals/modals.ts
+++ src/modals/modals.ts
@@ -6,12 +6,14 @@ type User = {
   recipitantId: string;
   notifyForEvents: Boolean;
   roles: Roles;
+  numberOfCmds: number;
 };
 
 const users = new Schema<User>({
   recipitantId: { type: String },
   notifyForEvents: { type: Boolean, default: true },
   roles: { type: String, default: "STUDENT" },
+  numberOfCmds: {type: Number, default: 0}
 });
 
 export const UserModel = model("Users", users);
