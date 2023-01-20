c0dec8876f052cbf6cdf27597aba1e26599a8788
Piyush Duggal <duggalpiyush0@gmail.com>
2023-01-20 11:30:26 +0530

    modified index

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,7 @@ import mongoose from "mongoose";
 // import { endOfToday } from "date-fns";
 import { sendAndDeleteMsg } from "./actions/sendAndDeleteMsg";
 import { pingEveryone } from "./actions/pingEveryone";
+import { connectToDb } from "./utils/db/connect";
 dotenv.config();
 
 // Initialized App
@@ -48,221 +49,210 @@ const DB_URL = LOCAL
   ? (process.env.DEV_DB_URL as string)
   : (process.env.PROD_DB_URL as string);
 
+// Connecting to DB
+(async () => {
+  await connectToDb(DB_URL);
+})();
+
+
 // Initializing Client
-mongoose
-  .connect(DB_URL)
-  .then(() => {
-    console.log("connected to DB");
-    const client = new Client({
-      puppeteer: {
-        headless: true,
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      },
-      authStrategy: new LocalAuth({
-        dataPath: `${__dirname}/sessions`,
-      }),
+const client = new Client({
+  puppeteer: {
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
+  authStrategy: new LocalAuth({
+    dataPath: `${__dirname}/sessions`,
+  }),
+});
+
+// For QR Code
+client.on("qr", (qr: string) => {
+  qrcode.generate(qr, { small: true });
+  console.log(qr);
+});
+
+// Event "READY"
+client.on("ready", async () => {
+  log({ msg: "Client Connected", type: "CONNECTED", error: false });
+  client.sendMessage(
+    process.env.WA_BOT_ID_DEV as string,
+    `${process.env.BOT_NAME as string}: I am Connected BOSS`
+  );
+});
+
+// Event "MESSAGE_CREATE"
+client.on("message_create", async (message: WAWebJS.Message) => {
+  // Check if message is from Group or Not, if yes, who contains whoean or userID
+  const who: MessageType = checkMessage(message);
+  // Mention Logic
+  const str: string[] = message.mentionedIds;
+  const isMention =
+    (message.body[0] === "@" && str.includes("919871453667@c.us")) ||
+    message.body
+      .toLowerCase()
+      .split(" ")
+      .includes(`@${(process.env.BOT_NAME as String).toLocaleLowerCase()}`);
+  if (isMention && who !== "NONE" && message.body.split(" ").length === 1) {
+    introduction(client, who, message);
+  }
+
+  let allChats = await client.getChats();
+  const WA_BOT: WA_Grp = allChats[BOT];
+
+  // Command check logic
+  if (
+    who !== "NONE" &&
+    COMMANDS_CMDS.includes(message.body.split(",")[0].toLocaleLowerCase())
+  ) {
+    sendCommands(client, message, who);
+  }
+
+  // Ping Everyone
+  if (who == "ADMIN" && ["everyone"].includes(message.body)) {
+    await pingEveryone(client, message);
+  }
+
+  // Checks if message's first letter is BOT_PREFIX
+  if (
+    who !== "NONE" &&
+    message.body[0] === (process.env.BOT_PREFIX as string)
+  ) {
+    await main(client, message, who);
+  }
+  // !@onlyUseOnce ONLY USE ONCE
+  if (who === "ADMIN" && message.body === "load") {
+    console.log(WA_BOT.participants);
+    WA_BOT.participants?.forEach(async (participant) => {
+      UserModel.create({
+        name: participant.id.user,
+        chatId: participant.id._serialized,
+      });
     });
-
-    // For QR Code
-    client.on("qr", (qr: string) => {
-      qrcode.generate(qr, { small: true });
-      console.log(qr);
-    });
-
-    // Event "READY"
-    client.on("ready", async () => {
-      log({ msg: "Client Connected", type: "CONNECTED", error: false });
-      client.sendMessage(
-        process.env.WA_BOT_ID_DEV as string,
-        `${process.env.BOT_NAME as string}: I am Connected BOSS`
-      );
-    });
-
-    // Event "MESSAGE_CREATE"
-    client.on("message_create", async (message: WAWebJS.Message) => {
-      // Check if message is from Group or Not, if yes, who contains whoean or userID
-      const who: MessageType = checkMessage(message);
-      // Mention Logic
-      const str: string[] = message.mentionedIds;
-      const isMention =
-        (message.body[0] === "@" && str.includes("919871453667@c.us")) ||
-        message.body
-          .toLowerCase()
-          .split(" ")
-          .includes(`@${(process.env.BOT_NAME as String).toLocaleLowerCase()}`);
-      if (isMention && who !== "NONE" && message.body.split(" ").length === 1) {
-        introduction(client, who, message);
-      }
-
-      let allChats = await client.getChats();
-      const WA_BOT: WA_Grp = allChats[BOT];
-
-      // Command check logic
-      if (
-        who !== "NONE" &&
-        COMMANDS_CMDS.includes(message.body.split(",")[0].toLocaleLowerCase())
-      ) {
-        sendCommands(client, message, who);
-      }
-
-      // Ping Everyone
-      if (who == "ADMIN" && ["everyone"].includes(message.body)) {
-        await pingEveryone(client, message);
-      }
-
-      // Checks if message's first letter is BOT_PREFIX
-      if (
-        who !== "NONE" &&
-        message.body[0] === (process.env.BOT_PREFIX as string)
-      ) {
-        await main(client, message, who);
-      }
-      // !@onlyUseOnce ONLY USE ONCE
-      if (who === "ADMIN" && message.body === "load") {
-        console.log(WA_BOT.participants);
-        WA_BOT.participants?.forEach(async (participant) => {
-          UserModel.create({
-            name: participant.id.user,
-            chatId: participant.id._serialized,
-          });
-        });
-        WA_BOT.sendMessage(
-          "SUCCESSFULLY ADDED ALL THE STUDENTS IN THE DB, MASTER!"
-        );
-      }
-    });
-
-    // Event "GROUP_JOIN"
-    client.on("group_join", async (msg: GroupNotification) => {
-      if (msg.chatId === (process.env.WA_BOT_ID as string)) {
-        log({
-          msg: `${msg.recipientIds[0]} Joined the Group`,
-          type: "GROUP_JOIN",
-          error: false,
-        });
-      }
-      if (msg.chatId === WA_BOT_ID) {
-        const contact = await client.getNumberId(msg.recipientIds[0]);
-        const details = await client.getContactById(contact?._serialized || "");
-        if (details.name) {
-          sendAndDeleteMsg(
-            client,
-            msg,
-            msg.recipientIds[0],
-            `${process.env.BOT_NAME as String}: *${
-              details.name
-            }* Thanks for joining the Group!\n${
-              USER_JOIN_GREETINGS.messages[
-                random(USER_JOIN_GREETINGS.messageNum)
-              ]
-            }\nHey new ${GREETINGS.member[random(GREETINGS.memberMsgNumber)]} ${
-              HEY_EMOJIES[random(HEY_EMOJIES.length)]
-            }!\nCheck out what bot(${
-              process.env.BOT_NAME as String
-            }) can do by *Mentioning* me!\nor check the Commands of ${
-              process.env.BOT_NAME as String
-            } by typing\n*${
-              process.env.BOT_PREFIX as string
-            }AllCmds*\n*IN THE GROUP*\nSimply watch the Video: https://drive.google.com/file/d/1tl33VralV0AXQ2EDJYnjC6r2eaCUHr-l/view?usp=sharing`
-          );
-
-          // const sticker = MessageMedia.fromFilePath(
-          //   `${__dirname}/assets/images/grpJoinLeaveImgs/${
-          //     grpJoinStickers.images[random(grpJoinStickers.numOfImgs)]
-          //   }.png`
-          // );
-          // client.sendMessage(msg.recipientIds[0], sticker, {
-          //   sendMediaAsSticker: true,
-          // });
-        } else {
-          sendAndDeleteMsg(
-            client,
-            msg,
-            msg.recipientIds[0],
-            `${process.env.BOT_NAME as String}: ${
-              GREETINGS.member[random(GREETINGS.memberMsgNumber)]
-            }, Thanks for Joining the Group!\n${
-              USER_JOIN_GREETINGS.messages[
-                random(USER_JOIN_GREETINGS.messageNum)
-              ]
-            }\nHey new ${GREETINGS.member[random(GREETINGS.memberMsgNumber)]} ${
-              HEY_EMOJIES[random(HEY_EMOJIES.length)]
-            }!\nCheck out what bot(${
-              process.env.BOT_NAME as String
-            }) can do by *Mentioning* me!\nor check the Commands of ${
-              process.env.BOT_NAME as String
-            } by typing\n*${
-              process.env.BOT_PREFIX as string
-            }AllCmds*\n*IN THE GROUP*\nSimply watch the Video: https://drive.google.com/file/d/1tl33VralV0AXQ2EDJYnjC6r2eaCUHr-l/view?usp=sharing`
-          );
-          // const sticker = MessageMedia.fromFilePath(
-          //   `${__dirname}/assets/images/grpJoinLeaveImgs/${
-          //     grpJoinStickers.images[random(grpJoinStickers.numOfImgs)]
-          //   }.png`
-          // );
-          // client.sendMessage(msg.recipientIds[0], sticker, {
-          //   sendMediaAsSticker: true,
-          // });
-        }
-      }
-    });
-
-    // GroupNotification {
-    //   id: {
-    //     fromMe: boolean,
-    //     remote: '1203630xxxxxxxxx@g.us',
-    //     id: '26650709261xxxxxxxxxx',
-    //     participant: '919990xxxxxxxx@c.us',
-    //     _serialized: 'false_12036xxxxxxxxxxxxx475@g.us_2665xxxxxxxxxxxx72395334_91xxxxxxxxxxxxx656@c.us'
-    //   },
-    //   body: '',
-    //   type: 'invite',
-    //   timestamp: 1672395334,
-    //   chatId: '1203630442xxxxxxxxx@g.us',
-    //   author: undefined,
-    //   recipientIds: [ '9199902xxxxxxxxx@c.us' ]
-    // }
-    client.on(
-      "group_leave",
-      async (notification: WAWebJS.GroupNotification) => {
-        let grpId = notification.chatId;
-        if (grpId === (process.env.WA_BOT_ID as string)) {
-          log({
-            msg: `${notification.recipientIds[0]} left the Group`,
-            type: "GROUP_LEFT",
-            error: false,
-          });
-        }
-        const sticker = MessageMedia.fromFilePath(
-          `${__dirname}/../src/assets/images/grpJoinLeaveImgs/${
-            grpLeaveStickers.images[random(grpLeaveStickers.numOfImgs)]
-          }.png`
-        );
-        if (notification.chatId === WA_BOT_ID) {
-          const allChats = await client.getChats();
-          const WA_BOT = allChats[BOT];
-          WA_BOT.sendMessage(
-            `${process.env.BOT_NAME as String}: somebody left`
-          );
-          WA_BOT.sendMessage(sticker, { sendMediaAsSticker: true });
-        }
-        await UserModel.findOneAndDelete({ chatId: notification.chatId });
-      }
+    WA_BOT.sendMessage(
+      "SUCCESSFULLY ADDED ALL THE STUDENTS IN THE DB, MASTER!"
     );
+  }
+});
 
-    // For checking the classes
-    // setInterval(async () => {
-    //   const chats = await client.getChats();
-    //   const WA_BOT: WA_Grp = chats[BOT];
-    //   sendClassNotification(WA_BOT);
-    //   log({ msg: "Checked", type: "INFO", error: false });
-    // }, 5 * 60 * 1000); // every 5 minutes
+// Event "GROUP_JOIN"
+client.on("group_join", async (msg: GroupNotification) => {
+  if (msg.chatId === (process.env.WA_BOT_ID as string)) {
+    log({
+      msg: `${msg.recipientIds[0]} Joined the Group`,
+      type: "GROUP_JOIN",
+      error: false,
+    });
+  }
+  if (msg.chatId === WA_BOT_ID) {
+    const contact = await client.getNumberId(msg.recipientIds[0]);
+    const details = await client.getContactById(contact?._serialized || "");
+    if (details.name) {
+      sendAndDeleteMsg(
+        client,
+        msg,
+        msg.recipientIds[0],
+        `${process.env.BOT_NAME as String}: *${
+          details.name
+        }* Thanks for joining the Group!\n${
+          USER_JOIN_GREETINGS.messages[random(USER_JOIN_GREETINGS.messageNum)]
+        }\nHey new ${GREETINGS.member[random(GREETINGS.memberMsgNumber)]} ${
+          HEY_EMOJIES[random(HEY_EMOJIES.length)]
+        }!\nCheck out what bot(${
+          process.env.BOT_NAME as String
+        }) can do by *Mentioning* me!\nor check the Commands of ${
+          process.env.BOT_NAME as String
+        } by typing\n*${
+          process.env.BOT_PREFIX as string
+        }AllCmds*\n*IN THE GROUP*\nSimply watch the Video: https://drive.google.com/file/d/1tl33VralV0AXQ2EDJYnjC6r2eaCUHr-l/view?usp=sharing`
+      );
 
-    client.initialize();
-  })
-  .catch((err: any) => {
-    log({ msg: err, type: "ERROR", error: true });
-  });
+      // const sticker = MessageMedia.fromFilePath(
+      //   `${__dirname}/assets/images/grpJoinLeaveImgs/${
+      //     grpJoinStickers.images[random(grpJoinStickers.numOfImgs)]
+      //   }.png`
+      // );
+      // client.sendMessage(msg.recipientIds[0], sticker, {
+      //   sendMediaAsSticker: true,
+      // });
+    } else {
+      sendAndDeleteMsg(
+        client,
+        msg,
+        msg.recipientIds[0],
+        `${process.env.BOT_NAME as String}: ${
+          GREETINGS.member[random(GREETINGS.memberMsgNumber)]
+        }, Thanks for Joining the Group!\n${
+          USER_JOIN_GREETINGS.messages[random(USER_JOIN_GREETINGS.messageNum)]
+        }\nHey new ${GREETINGS.member[random(GREETINGS.memberMsgNumber)]} ${
+          HEY_EMOJIES[random(HEY_EMOJIES.length)]
+        }!\nCheck out what bot(${
+          process.env.BOT_NAME as String
+        }) can do by *Mentioning* me!\nor check the Commands of ${
+          process.env.BOT_NAME as String
+        } by typing\n*${
+          process.env.BOT_PREFIX as string
+        }AllCmds*\n*IN THE GROUP*\nSimply watch the Video: https://drive.google.com/file/d/1tl33VralV0AXQ2EDJYnjC6r2eaCUHr-l/view?usp=sharing`
+      );
+      // const sticker = MessageMedia.fromFilePath(
+      //   `${__dirname}/assets/images/grpJoinLeaveImgs/${
+      //     grpJoinStickers.images[random(grpJoinStickers.numOfImgs)]
+      //   }.png`
+      // );
+      // client.sendMessage(msg.recipientIds[0], sticker, {
+      //   sendMediaAsSticker: true,
+      // });
+    }
+  }
+});
+
+// GroupNotification {
+//   id: {
+//     fromMe: boolean,
+//     remote: '1203630xxxxxxxxx@g.us',
+//     id: '26650709261xxxxxxxxxx',
+//     participant: '919990xxxxxxxx@c.us',
+//     _serialized: 'false_12036xxxxxxxxxxxxx475@g.us_2665xxxxxxxxxxxx72395334_91xxxxxxxxxxxxx656@c.us'
+//   },
+//   body: '',
+//   type: 'invite',
+//   timestamp: 1672395334,
+//   chatId: '1203630442xxxxxxxxx@g.us',
+//   author: undefined,
+//   recipientIds: [ '9199902xxxxxxxxx@c.us' ]
+// }
+client.on("group_leave", async (notification: WAWebJS.GroupNotification) => {
+  let grpId = notification.chatId;
+  if (grpId === (process.env.WA_BOT_ID as string)) {
+    log({
+      msg: `${notification.recipientIds[0]} left the Group`,
+      type: "GROUP_LEFT",
+      error: false,
+    });
+  }
+  const sticker = MessageMedia.fromFilePath(
+    `${__dirname}/../src/assets/images/grpJoinLeaveImgs/${
+      grpLeaveStickers.images[random(grpLeaveStickers.numOfImgs)]
+    }.png`
+  );
+  if (notification.chatId === WA_BOT_ID) {
+    const allChats = await client.getChats();
+    const WA_BOT = allChats[BOT];
+    WA_BOT.sendMessage(`${process.env.BOT_NAME as String}: somebody left`);
+    WA_BOT.sendMessage(sticker, { sendMediaAsSticker: true });
+  }
+  await UserModel.findOneAndDelete({ chatId: notification.chatId });
+});
+
+// For checking the classes
+// setInterval(async () => {
+//   const chats = await client.getChats();
+//   const WA_BOT: WA_Grp = chats[BOT];
+//   sendClassNotification(WA_BOT);
+//   log({ msg: "Checked", type: "INFO", error: false });
+// }, 5 * 60 * 1000); // every 5 minutes
+
+client.initialize();
 
 // Get Bot LIVE
 // Continuously ping the server to prevent it from becoming idle
--- src/utils/db/connect.ts
+++ src/utils/db/connect.ts
@@ -1,8 +1,10 @@
-import { connect } from "mongoose";
+import { connect, set } from "mongoose";
 
 export const connectToDb = async (dbUrl: string) => {
   try {
+    set("strictQuery", false); // HACK: To suppress mongo deprecated warning
     await connect(dbUrl);
+    console.log("connected to db");
   } catch (error) {
     console.log(error);
     process.exit(0);
