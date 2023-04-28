57a0dd8afaa29cad5daae280788273225ad68ced
Piyush Duggal <duggalpiyush0@gmail.com>
2023-04-28 17:50:51 +0530

    added build

--- dist/actions/sendClassNotification.js
+++ dist/actions/sendClassNotification.js
@@ -1,10 +1,10 @@
-"use strict";var d=Object.create;var o=Object.defineProperty;var c=Object.getOwnPropertyDescriptor;var u=Object.getOwnPropertyNames;var l=Object.getPrototypeOf,C=Object.prototype.hasOwnProperty;var p=(e,a)=>{for(var n in a)o(e,n,{get:a[n],enumerable:!0})},r=(e,a,n,s)=>{if(a&&typeof a=="object"||typeof a=="function")for(let t of u(a))!C.call(e,t)&&t!==n&&o(e,t,{get:()=>a[t],enumerable:!(s=c(a,t))||s.enumerable});return e};var h=(e,a,n)=>(n=e!=null?d(l(e)):{},r(a||!e||!e.__esModule?o(n,"default",{value:e,enumerable:!0}):n,e)),g=e=>r(o({},"__esModule",{value:!0}),e);var b={};p(b,{sendClassNotification:()=>A});module.exports=g(b);var i=h(require("date-fns/isToday")),f=require("date-fns"),m=require("../resources/calendar");const y=e=>{let a=[];const n=new Date;return e.forEach(s=>{if((0,i.default)(s.date)){const t=(0,f.differenceInMinutes)(s.date,n);t<5&&0<t?a.push({...s,numberOfMinutes:5}):t<10&&0<t&&a.push({...s,numberOfMinutes:10})}}),a},A=e=>{const a=y(m.CALENDAR);if(a.length){let n=`\u26A0 Attention Guys!! \u26A0 CLASS!
+"use strict";var d=Object.create;var o=Object.defineProperty;var c=Object.getOwnPropertyDescriptor;var l=Object.getOwnPropertyNames;var u=Object.getPrototypeOf,C=Object.prototype.hasOwnProperty;var p=(e,n)=>{for(var s in n)o(e,s,{get:n[s],enumerable:!0})},r=(e,n,s,t)=>{if(n&&typeof n=="object"||typeof n=="function")for(let a of l(n))!C.call(e,a)&&a!==s&&o(e,a,{get:()=>n[a],enumerable:!(t=c(n,a))||t.enumerable});return e};var g=(e,n,s)=>(s=e!=null?d(u(e)):{},r(n||!e||!e.__esModule?o(s,"default",{value:e,enumerable:!0}):s,e)),h=e=>r(o({},"__esModule",{value:!0}),e);var b={};p(b,{sendClassNotification:()=>A});module.exports=h(b);var i=g(require("date-fns/isToday")),f=require("date-fns"),m=require("../resources/calendar");const y=e=>{let n=[];const s=new Date;return e.forEach(t=>{if((0,i.default)(t.date)){const a=(0,f.differenceInMinutes)(t.date,s);a<5&&0<a?n.push({...t,numberOfMinutes:5}):a<10&&0<a&&n.push({...t,numberOfMinutes:10})}}),n},A=(e,n)=>{const s=y(m.CALENDAR);if(s.length){let t=`\u26A0 Attention Guys!! \u26A0 CLASS!
 
-\u{1F4D8}*Today's Classes*\u{1F4D8}`;a.forEach(s=>n+=`
+\u{1F4D8}*Today's Classes*\u{1F4D8}`;s.forEach(a=>t+=`
  -------------------------------- 
-\u{1F4D6} *Topic*      : *${s.topic}* 
-\u{1F570} *Time*   : _Starting in *${s.numberOfMinutes}* minutes_
+\u{1F4D6} *Topic*      : *${a.topic}* 
+\u{1F570} *Time*   : _Starting in *${a.numberOfMinutes}* minutes_
 \u{1F4C5} *Date*       : *Today!* 
-\u{1F3EB} *Course*  : ${s.courseName}
-*Calendar Link*: https://calendar.google.com/calendar/u/0/r/day `),e.sendMessage(n)}};0&&(module.exports={sendClassNotification});
+\u{1F3EB} *Course*  : ${a.courseName}
+*Calendar Link*: https://calendar.google.com/calendar/u/0/r/day `),e.sendMessage(n,t)}};0&&(module.exports={sendClassNotification});
 //# sourceMappingURL=sendClassNotification.js.map
