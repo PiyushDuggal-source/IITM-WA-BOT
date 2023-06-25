7de548f9d2b5244b229b5f24288320f22894b580
Piyush Duggal <duggalpiyush0@gmail.com>
2023-06-25 21:01:04 +0530

    added types

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+import { MessageContent } from "whatsapp-web.js"
+
+export type MessageBody = {
+  message: MessageContent,
+  chatId: string,
+}
+
+export type ResType = {
+  statusCode: number,
+  error?: any,
+}
