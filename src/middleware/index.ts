a8cd78a05d5d69da814e6a83a8f820ce5e425378
Piyush Duggal <duggalpiyush0@gmail.com>
2023-06-25 23:59:09 +0530

    added middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 
 export const checkAPI = (req: Request, res: Response, next: NextFunction) => {
-  console.log("Entering checkAPI");
+  console.log("\nEntering checkAPI");
   if (req.headers["x-bot-api"] !== process.env.BOT_API_KEY) {
     res.status(401).send("Unauthorized");
     return;
   }
   next();
-  console.log("Leaving checkAPI");
+  console.log("Leaving checkAPI\n");
 };
