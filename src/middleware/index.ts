2304855b7759443d2009db51c603f46f97b7b6e9
Piyush Duggal <duggalpiyush0@gmail.com>
2023-08-25 03:50:49 +0530

    fixed API env value

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 export const checkAPI = (req: Request, res: Response, next: NextFunction) => {
   console.log("\nEntering checkAPI");
-  if (req.headers["x-bot-api"] !== process.env.BOT_API_KEY) {
+  if (req.headers["x-bot-api"] !== process.env.BOT_API) {
     res.status(401).send("Unauthorized");
     return;
   }
