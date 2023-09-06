a33eb9c71227537955bb28e4959d0822cc145b90
Piyush Duggal_Mr_Unchained <piyushduggal@pop-os.localdomain>
2023-09-06 17:18:50 +0530

    username check

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,6 @@ export const WA_BOT_ID = LOCAL
 //   : (process.env.PROD_DB_URL as string);
 
 // Connect To DB
-
 // Event "READY
 client.on("ready", async () => {
   console.log("Client is ready");
